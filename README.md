869c7657f5b5b4fbce67fcca8248d23636b9f5d0
Tesimune <tesimune@gmail.com>
2026-09-25 13:49:41 +0100

    Remove JavaScript from skills list and update README to reflect changes in tech stack

--- README.md
+++ README.md
@@ -17,6 +17,8 @@
 ###
 
 <div data-importer="techs" align="center">
+  <!-- <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" height="40" alt="javascript logo"  />
+  <img width="12" /> -->
   <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg" height="40" alt="typescript logo"  />
   <img width="12" />
   <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg" height="40" alt="python logo"  />
--- src/lib/data/skills.ts
+++ src/lib/data/skills.ts
@@ -4,10 +4,10 @@ interface ISkills {
 }
 
 export const coreStack: ISkills[] = [
-  {
-    name: 'JavaScript',
-    src: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg',
-  },
+  // {
+  //   name: 'JavaScript',
+  //   src: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg',
+  // },
   {
     name: 'TypeScript',
     src: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg',
@@ -15,6 +15,10 @@ export const coreStack: ISkills[] = [
   {
     name: 'Python',
     src: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg',
+  },
+  {
+    name: 'Java',
+    src: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/java/java-original.svg',
   },
   {
     name: 'PHP',
